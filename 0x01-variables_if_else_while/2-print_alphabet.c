ca1a31c7757f10a6f2981934c2942d1a76b0836c
Mohamed Mohsen <mohamedmohsen4b@gmail.com>
2023-10-08 05:53:25 -0700

    c programming

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,13 @@
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char ch = 'a';
 
-	for (i = 0; i < 26; i++)
+	while (ch <= 'z')
 	{
-		putchar(alp[i]);
+		putchar(ch);
+		ch++;
 	}
 	putchar('\n');
 	return (0);
 }
-		
