2bb84ccf466e616a22a60132ff81aea65fa54787
Mohamed Mohsen <mohamedmohsen4b@gmail.com>
2023-11-18 16:30:10 -0800

    readme

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,14 @@
-#include "function_pointers.h"
-#include <stdio.h>
-/**
-* print_name - print name using pointer to function
-* @name: string to add
-* @f: pointer to function
-* Return: nothing
-**/
-void print_name(char *name, void (*f)(char *))
-{
-if (name == NULL || f == NULL)
-return;
-(name);
-}
+#ifndef FUNCTION_POINTERS_
+#define FUNCTION_POINTERS_
+
+
+#include <stddef.h>
+
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+
+#endif /* FUNCTION_POINTERS_H*/
